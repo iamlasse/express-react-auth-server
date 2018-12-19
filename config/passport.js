34c1fb7d158c25ac3b36a86ea3bf6e7f811671d7
Lasse Larsen <>
2018-12-19 15:54:36 +0100

    Refactor Auth and Passport

--- config/passport.js
+++ config/passport.js
@@ -1,14 +1,13 @@
-const passport = require('passport');
-const User = require('../modules/users/model');
-const passportJWT = require('passport-jwt');
-const JWTStrategy = passportJWT.Strategy;
-const ExtractJWT = passportJWT.ExtractJwt;
-const LocalStrategy = require('passport-local').Strategy;
-var localLogin = require('./strategies/localLogin');
-var localSignUp = require('./strategies/localSignUp');
-var facebookAuth = require('./strategies/facebookAuth');
-var googleAuth = require('./strategies/googleAuth');
-const config = require('../config');
+const passport = require('passport')
+const User = require('../modules/users/model')
+const passportJWT = require('passport-jwt')
+const JWTStrategy = passportJWT.Strategy
+const ExtractJWT = passportJWT.ExtractJwt
+var localLogin = require('./strategies/localLogin')
+var localSignUp = require('./strategies/localSignUp')
+var facebookAuth = require('./strategies/facebookAuth')
+var googleAuth = require('./strategies/googleAuth')
+const config = require('../config')
 
 passport.use(
 	'jwt',
@@ -18,41 +17,33 @@ passport.use(
 			secretOrKey: config.secret
 		},
 		async function(jwtPayload, done) {
-			console.log('Authenticate user with JWT: ', jwtPayload);
-
 			// find the user in db if needed
 			try {
-				const user = await User.findById(jwtPayload.sub, 'local.email local.username');
+				const user = await User.findById(jwtPayload.sub, 'local.email local.username')
 
-				console.log(user);
+				if (!user) return done(null, false, { message: 'No User found...' })
 
-				if (!user) return done(null, false, { message: 'No User found...' });
-
-				return done(null, user);
+				return done(null, user)
 			} catch (error) {
-				return done(error, false);
+				return done(error, false)
 			}
 		}
 	)
-);
+)
 
 // used to serialize the user for the session
 passport.serializeUser(function(user, done) {
-	console.log('Serialize user: ', user);
-
-	done(null, user);
-});
+	done(null, user)
+})
 
 // used to deserialize the user
 passport.deserializeUser(function(id, done) {
-	console.log('Deserialize User: ', id);
-
 	User.findById(id).exec(function(err, user) {
-		done(err, user);
-	});
-});
+		done(err, user)
+	})
+})
 
-localLogin(passport);
-localSignUp(passport);
-googleAuth(passport);
-facebookAuth(passport);
+localLogin(passport)
+localSignUp(passport)
+googleAuth(passport)
+facebookAuth(passport)
